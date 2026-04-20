f64a8b0dde19cd839ee7e31235571122145bf71a
KeiranCPFlynn <96777179+KeiranCPFlynn@users.noreply.github.com>
2026-04-20 11:05:04 +0700

    rollback redirect fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.llmnesia.com' }],
-        destination: 'https://llmnesia.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
